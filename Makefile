1ca76cc0c3b176e139c64019ff42bc9a1682e70d
garther <piatek.mar@gmail.com>
2019-05-07 20:41:00 +0200

    initial commit

--- Makefile
+++
@@ -1,5 +0,0 @@
-deploy:
-	if [ -d dist ]; then rm -rf dist; fi
-	python setup.py bdist_wheel
-	twine upload dist/*
-
--- waterpictrl/waterpictrl.py
+++ waterpictrl/waterpictrl.py
@@ -4,37 +4,51 @@ import socket
 import sys
 import traceback
 import paho.mqtt.client as mqtt
+import threading
 
 GPIO.setmode(GPIO.BCM)
+RELAY_ON = GPIO.LOW
+RELAY_OFF = GPIO.HIGH
+CUTOFF = 30
 
+class Outlet:
+    gpio_pin = None
+    topic_name = None
 
-class Switch:
-    gpio = None
-    topic_status = None
-
-    def __init__(self, gpio, topic_status):
-        self.gpio = gpio
-        self.topic_status = topic_status
-        GPIO.setup(self.gpio, GPIO.OUT)
+    def __init__(self, gpio_pin, topic_name):
+        self.gpio_pin = gpio_pin
+        self.topic_name = topic_name
+        GPIO.setup(self.gpio_pin, GPIO.OUT)
         self.off()
 
     def on(self):
-        GPIO.output(self.gpio, GPIO.HIGH)
+        GPIO.output(self.gpio_pin, RELAY_ON)
 
     def off(self):
-        GPIO.output(self.gpio, GPIO.LOW)
+        GPIO.output(self.gpio_pin, RELAY_OFF)
 
+    def _func_to_be_threaded(self):
+        i = 0
+        while i < CUTOFF:
+            my_state=GPIO.input(self.gpio_pin)
+            print('outlet is ', my_state)
+            if my_state == 1:
+                print('outlet closed, exiting')
+                break
+            time.sleep(1)
+            i += 1
+        self.off()
 
-class Switcher:
+class WaterPiCtrl:
     config = None
     mqtt_client = None
     mqtt_connected = False
-    switches = {}
+    outlets = {}
 
     def __init__(self, config):
         self.config = config
-        for switch_cfg in self.config['switches']:
-            self.switches[switch_cfg['topic_set']] = Switch(int(switch_cfg['gpio']), switch_cfg['topic_status'])
+        for switch_cfg in self.config['outlets']:
+            self.outlets[switch_cfg['topic_set']] = Outlet(int(switch_cfg['gpio_pin']), switch_cfg['topic_name'])
 
     def verbose(self, message):
         if self.config and 'verbose' in self.config and self.config['verbose'] == 'true':
@@ -58,7 +72,7 @@ class Switcher:
 
             try:
                 self.mqtt_client.connect(self.config['mqtt_host'], int(self.config['mqtt_port']), 10)
-                for switch_cfg in self.config['switches']:
+                for switch_cfg in self.config['outlets']:
                     self.mqtt_client.subscribe(switch_cfg['topic_set'], 0)
                 self.mqtt_client.loop_forever()
             except:
@@ -94,15 +108,27 @@ class Switcher:
 
     def mqtt_on_message(self, client, userdata, message):
         self.verbose(message.topic)
+        if message.topic in self.outlets:
+            value = str(message.payload.decode("utf-8"))
+            if value == 'ON':
+                self.outlets[message.topic].on()
+                self.publish_status(self.outlets[message.topic].topic_name, 'ON')
+                threading.Thread(target=self._func_to_be_threaded, args=[message.topic]).start()
+            elif value == 'OFF':
+                self.outlets[message.topic].off()
+                self.publish_status(self.outlets[message.topic].topic_name, 'OFF')
 
-        if message.topic in self.switches:
-            value = str(message.payload)
-            if value == 'on':
-                self.switches[message.topic].on()
-                self.publish_status(self.switches[message.topic].topic_status, 'on')
-            elif value == 'off':
-                self.switches[message.topic].off()
-                self.publish_status(self.switches[message.topic].topic_status, 'off')
+    def _func_to_be_threaded(self,topic):
+        i = 0
+        while i < CUTOFF:
+            my_state=GPIO.input(self.outlets[topic].gpio_pin)
+            if my_state == 1:
+                print('outlet closed, exiting')
+                break
+            time.sleep(1)
+            i += 1
+        self.outlets[topic].off()
+        self.publish_status(self.outlets[topic].topic_name, 'OFF')
 
     def publish_status(self, topic, status):
         if self.mqtt_connected:
